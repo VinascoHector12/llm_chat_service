586ce55fd339a186b8a6ec7f8b491ee0e773b781
Héctor <hectorvinasco12@gmail.com>
2026-03-20 11:32:07 -0500

    Ajuste kluster aws

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,8 +3,9 @@ import healthRoutes from './healthRoutes.js';
 import chatRoutes from './chatRoutes.js';
 
 const router = express.Router();
+const prefix = '/llm';
 
-router.use('/health', healthRoutes);
-router.use('/v1/chat', chatRoutes);
+router.use(`${prefix}/health`, healthRoutes);
+router.use(`${prefix}/v1/chat`, chatRoutes);
 
 export default router;
